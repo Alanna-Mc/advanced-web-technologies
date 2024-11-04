121f3f5824a1f6781875849085f886f731ae6c5d
Alanna Mc <Alanna-Mc@users.noreply.github.com>
2024-11-04 00:16:36 +0000

    Adding new routes and redirects

--- hello.py
+++ hello.py
@@ -2,6 +2,25 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route ("/")
+def root():
+  return "The default, 'root' route"
+
+@app.route('/hello/')
 def hello_world():
-    return 'Hello Napier'
+    return "Hello Napier!!! :D"
+
+@app.route("/goodbye/")
+def goodbye():
+  return "Goodbye Everyone :-^"
+
+@app.route('/force404')
+def force404():
+  abort(404)
+
+@app.errorhandler(404)
+def page_not_found(error):
+  return "Couldn't find the page you requested.", 404
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
