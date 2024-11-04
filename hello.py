f37135966d5685a1586fb43b6aa03de856684774
Alanna Mc <Alanna-Mc@users.noreply.github.com>
2024-11-04 17:15:55 +0000

    Requests & URL  variables

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, url_for
+from flask import Flask, url_for, request
 app = Flask(__name__)
 
 @app.route ("/")
@@ -41,5 +41,31 @@ def force404():
 def page_not_found(error):
   return "Couldn't find the page you requested.", 404
 
+@app.route("/account", methods=['GET', 'POST'])
+def account():
+  if request.method == 'POST':
+    print (request.form)
+    name = request.form["name"]
+    return "hello %s" % name
+  else:
+   page = """
+   <html><body>
+     <form action= "" method="post" name="form">
+       <label for="name">Name:</label>
+       <input type = "text" name ="name" id="submit"/>
+       <input type="submit" name="submit" id="submit"/>
+     </form>
+     </body><html>"""
+
+   return page
+
+@app.route("/hello/<name>")
+def hello(name):
+  return "Hello %s" % name
+
+@app.route("/add/<int:first>/<int:second>")
+def add(first, second):
+  return str(first+second)
+
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+  app.run(host='0.0.0.0',port=5001, debug=True)
